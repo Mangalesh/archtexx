2c329051b0527250ae5f77f6dbaab08080cb66bd
Mangalesh <mangalesh2006@gmail.com>
2016-04-21 14:01:46 +0200

    trail.php added for apache testing

--- trail.php
+++ trail.php
@@ -1,11 +1,10 @@
 <?php
 
-echo " this is for testing today ";
-echo " great its working";
-echo " Welcome to New Users for accesss";
 
 
-echo "Its in my new trail.. thanks"
+
+echo " i am testing for apache directory copy";
+
 
 ?>
 
